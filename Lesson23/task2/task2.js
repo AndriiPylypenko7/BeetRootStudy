7c04678ff0b3bef266f69004632e10b759b325c3
AndriiPylypenko7 <andruxan1989@gmail.com>
2021-04-13 18:40:15 +0300

    hw23fix

--- Lesson23/task2/task2.js
+++ Lesson23/task2/task2.js
@@ -14,60 +14,49 @@ button.onclick = function () {
       console.log("!");
       document.querySelector(".result").innerHTML = "!";
       break;
-  }
-  switch (num) {
+
     case 2:
       console.log("@");
       document.querySelector(".result").innerHTML = "@";
       break;
-  }
-  switch (num) {
+
     case 3:
       console.log("#");
       document.querySelector(".result").innerHTML = "#";
       break;
-  }
-  switch (num) {
+
     case 4:
       console.log("$");
       document.querySelector(".result").innerHTML = "$";
       break;
-  }
-  switch (num) {
+
     case 5:
       console.log("%");
       document.querySelector(".result").innerHTML = "%";
       break;
-  }
-  switch (num) {
+
     case 6:
       console.log("^");
       document.querySelector(".result").innerHTML = "^";
       break;
-  }
-  switch (num) {
+
     case 7:
       console.log("&");
       document.querySelector(".result").innerHTML = "&";
       break;
-  }
-  switch (num) {
+
     case 8:
       console.log("*");
       document.querySelector(".result").innerHTML = "*";
       break;
-  }
-  switch (num) {
+
     case 9:
       console.log("(");
       document.querySelector(".result").innerHTML = "(";
       break;
-  }
-  if (num > 9) {
-    console.log("Введите число от 0 до 9!!!");
-    document.querySelector(".result").innerHTML = "Введите число от 0 до 9!!!";
-  } else if (num < 0) {
-    console.log("Введите число от 0 до 9!!!");
-    document.querySelector(".result").innerHTML = "Введите число от 0 до 9!!!";
+    default:
+      console.log("Введите число от 0 до 9!!!");
+      document.querySelector(".result").innerHTML =
+        "Введите число от 0 до 9!!!";
   }
 };
